5f79feaf07de37b53f3167308ac034fd8c9bf62d
dganh <duonganh2973@gmail.com>
2022-06-17 14:10:06 +0700

    rm noise

--- hw2/src/main.rs
+++ hw2/src/main.rs
@@ -70,23 +70,19 @@
 // }
 
 
-// //Exercise 4
-// fn main(){
-//     let  mut a = vec![1,2,3,4,5];
-//     let  mut i = 0;
-//     let c = 0;
-//     let (b, c) = test(&mut a);
-//     println!("{:?}",b);
-// }
+//Exercise 4
+fn main(){
+    let  mut a = vec![1,2,3,4,5];
+    let b = test(&mut a);
+    println!("{:?}",b);
+}
 
-// fn test( a: &mut Vec<u8>) -> (Vec<u8>, i32) {
-//     let mut b:Vec<u8>  = Vec::new();
-//     let mut c:u8 = 0;
-//     loop {
-//         if a.len() == 0 { break; }
-//         let d = a.pop().unwrap();
-//         c = c+d;
-//         b.push(d);
-//     }
-//     (b, c as i32)
-// }
+fn test( a: &mut Vec<u8>) -> Vec<u8> {
+    let mut b:Vec<u8>  = Vec::new();
+    loop {
+        if a.len() == 0 { break; }
+        let d = a.pop().unwrap();
+        b.push(d);
+    }
+    b
+}
