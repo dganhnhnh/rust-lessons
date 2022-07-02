2e5174f4d5367ce424dc152004b21ebd0651825a
dganh <duonganh2973@gmail.com>
2022-07-02 15:45:47 +0700

    done hw3

--- hw3/src/main.rs
+++ hw3/src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+#[derive(Debug)]
 pub struct School {
     students: HashMap<String, u32>,
 }
@@ -13,20 +13,29 @@ impl School {
 
     pub fn add(&mut self, grade: u32, student: &str) {
         // unimplemented!()
-        
-        
+        self.students.insert(student.to_string(), grade);
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        unimplemented!()
-
+        let mut grades_result=Vec::new();
+        for (student, grade) in &self.students{
+            grades_result.push(*grade);
+        }
+        grades_result.sort();
+        grades_result.dedup();
+        grades_result
     }
 
 
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        unimplemented!()
-
-        
+        let mut same_grades=Vec::new();
+        for (student, stu_grade) in &self.students{
+            if *stu_grade == grade {
+                same_grades.push(student.to_string());
+            }
+        }
+        same_grades.sort();
+        same_grades
     }
 }
 
